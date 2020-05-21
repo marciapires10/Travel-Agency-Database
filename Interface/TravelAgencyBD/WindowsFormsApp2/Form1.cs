4f8b7560365991b83de93ff48178fe4e7cb2871b
marciapires10 <35606627+marciapires10@users.noreply.github.com>
2020-05-21 15:43:47 +0100

    Minor changes Form1

--- Interface/TravelAgencyBD/WindowsFormsApp2/Form1.cs
+++ Interface/TravelAgencyBD/WindowsFormsApp2/Form1.cs
@@ -18,7 +18,6 @@ namespace WindowsFormsApp2
         public Form1()
         {
             this.AutoSize = true;
-            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             InitializeComponent();
         }
 
