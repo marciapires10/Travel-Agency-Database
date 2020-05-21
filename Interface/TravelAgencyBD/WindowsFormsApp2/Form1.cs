72e4bf4da036c31b36916da4abc0375e3b44fe89
marciapires10 <35606627+marciapires10@users.noreply.github.com>
2020-05-21 15:28:27 +0100

    Major changes at Interface: login successful

---
+++ Interface/TravelAgencyBD/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection cn;
+
+        public Form1()
+        {
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = getSGBDConnection();
+        }
+
+        private SqlConnection getSGBDConnection()
+        {
+            return new SqlConnection("Data Source=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Initial Catalog=p1g9;Persist Security Info=True;User ID=p1g9;Password=4rmariO");
+        }
+
+        private bool verifySGBDConnection()
+        {
+            if(cn == null)
+            {
+                cn = getSGBDConnection();
+            }
+
+            if (cn.State != ConnectionState.Open)
+            {
+                cn.Open();
+            }
+
+            return cn.State == ConnectionState.Open;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!verifySGBDConnection())
+                return;
+
+            //cn = getSGBDConnection();
+
+
+            string email = textBox1.Text;
+            string password = textBox2.Text;
+
+            if(email == "")
+            {
+                MessageBox.Show("Please enter e-mail", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(password == "")
+            {
+                MessageBox.Show("Please enter password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand("SELECT Email, Password FROM TravelAgency.Agent WHERE Email= @Email AND Password= @Password", cn);
+
+            SqlParameter aEmail = new SqlParameter("@Email", SqlDbType.VarChar);
+            SqlParameter aPassword = new SqlParameter("@Password", SqlDbType.VarChar);
+
+            cmd.Parameters.Add(aEmail);
+            cmd.Parameters.Add(aPassword);
+
+            cmd.Parameters["@Email"].Value = email;
+            cmd.Parameters["@Password"].Value = password;
+
+            cmd.Connection = cn;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                MessageBox.Show("You have logged in successfully" + email);
+                this.Hide();
+            }
+
+            else
+            {
+                MessageBox.Show("Login failed, try again", "Login Denied", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            cn.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
